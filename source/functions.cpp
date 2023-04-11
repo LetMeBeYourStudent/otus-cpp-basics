020b0be380f341eb5e066efb97f0ebe4e3df8f3f
xenonex-ru <xenonex-ru@yandex.ru>
2023-04-11 11:13:05 +0300

    Первое задание
    Решение первой практической работы

--- source/functions.cpp
+++ source/functions.cpp
@@ -1,6 +1,10 @@
 #include "../headers/functions.h"
+#include <iostream>
 
 std::string print_hello_worlds()
 {
-    return "Добро пожаловать в прекрасный мир С++";
+    std::string your_name;
+    std::cout << "Введите свое имя: ";
+    std::cin >> your_name;
+    return "Добро пожаловать в прекрасный мир С++, " + your_name;
 }
